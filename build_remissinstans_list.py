f05d1146370d277a85f571e0114129a56279749c
Pierre Mesure <pierre.mesure@gmail.com>
2019-02-11 22:37:50 -0800

    Adding UML and cleaning a file

--- build_remissinstans_list.py
+++ build_remissinstans_list.py
@@ -1,15 +1,8 @@
-import urllib.request
-import os
-
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.pdfparser import PDFSyntaxError
 
-from database.file import File
 from database.document import Document
-from database.consultee_list import ConsulteeList
 from database.consultee import Consultee
-from database.answer import Answer
-from database.remiss import Remiss
 
 from service.downloader import Downloader
 from service.document_parser import DocumentParser
@@ -25,16 +18,9 @@ if RESET_DB:
     Database.commit()
     print(f'Emptied the consultee table.\n')
 
-saved_consultee_lists = ConsulteeList.query.all()
-saved_consultees = Consultee.query.all()
-saved_remisser = Remiss.query.all()
 saved_documents = Document.query.filter(Document.type == 'consultee_list')
 
-nb_of_consultees = len(saved_consultees)
-print(f'Found {nb_of_consultees} consultees in the database.')
-
 for document in saved_documents:
-
     if not RESET_DB and document.consultee_list != []:
         print(f'Consultees for remiss {document.remiss_id} already in database.')
         continue
@@ -47,7 +33,7 @@ for document in saved_documents:
         try:
             f = Downloader.get(document.files[0].url)
         except urllib.error.HTTPError:
-            print(f'404: Remissinstans {document.remiss_id} not found.')
+            print(f'404: File for Remiss {document.remiss_id} not found.')
 
         if f is not None:
             fp = BytesIO(f)
@@ -61,11 +47,11 @@ for document in saved_documents:
     try:
         list = DocumentParser.extract_list(fp)
     except (PDFTextExtractionNotAllowed, PDFSyntaxError):
-        print(f'Document {document.remiss_id} could not be extracted.')
+        print(f'Document for Remiss {document.remiss_id} could not be extracted.')
         continue
 
     if not list:
-        print(f'Document {document.remiss_id} could not be extracted.')
+        print(f'Document for Remiss {document.remiss_id} could not be extracted.')
         continue
 
     document.consultee_list = list
