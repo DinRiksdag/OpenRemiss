d83cd2d79248e6e3dde730b9cc18132a19b7cff2
Pierre Mesure <pierre@mesu.re>
2022-11-12 21:02:36 +0100

    Committing the previous cleaning efforts...

--- clean_data.py
+++ clean_data.py
@@ -10,33 +10,47 @@ saved_answers = Answer.query.all()
 
 RESET_DB = False
 
-print('II-1 Cleaning file names...')
+print('II-1 Light cleaning file names...')
 for remiss_index, remiss in enumerate(saved_remisser, start=1):
     answers_for_remiss = Answer.query.filter_by(remiss_id=remiss.id).all()
 
     nb_of_remisser = len(saved_remisser)
 
     for answer in answers_for_remiss:
-        if RESET_DB or answer.organisation is None:
-            org_name = answer.files[0].name
+        org_name = answer.files[0].name
 
-            if len(answers_for_remiss) > 3:
-                filenames = [a.files[0].name for a in answers_for_remiss]
+        if len(answers_for_remiss) > 3:
+            filenames = [a.files[0].name for a in answers_for_remiss]
 
-                common = Cleaner.long_substr(filenames)
+            common = Cleaner.long_substr(filenames)
 
-                if len(common) > 3:
-                    org_name = org_name.replace(common, '')
+            if len(common) > 3:
+                org_name = org_name.replace(common, '')
 
-            org_name = Cleaner.get_organisation_name(org_name)
+        org_name = Cleaner.light_clean(org_name)
 
-            answer.organisation = org_name
-            Database.commit()
+        answer.organisation = org_name
+        Database.commit()
+    print(f'{remiss_index}/{nb_of_remisser} - Cleaned')
+
+print('II-1 Deep cleaning file names...')
+for remiss_index, remiss in enumerate(saved_remisser, start=1):
+    answers_for_remiss = Answer.query.filter_by(remiss_id=remiss.id).all()
+
+    nb_of_remisser = len(saved_remisser)
+
+    for answer in answers_for_remiss:
+        org_name = answer.organisation
+
+        org_name = Cleaner.deep_clean(org_name)
+
+        answer.organisation = org_name
+        Database.commit()
     print(f'{remiss_index}/{nb_of_remisser} - Cleaned')
 
 saved_lists = Document.query.filter(Document.type == 'consultee_list').all()
 
-print('II-2 Cleaning organisation names from consultee lists...')
+print('II-2 Light cleaning organisation names from consultee lists...')
 for document_index, consultee_list in enumerate(saved_lists, start=1):
     consultees_for_list = Consultee.query.filter_by(
                                 consultee_list_id=consultee_list.id
@@ -45,11 +59,27 @@ for document_index, consultee_list in enumerate(saved_lists, start=1):
     nb_of_consultee_lists = len(saved_lists)
 
     for consultee in consultees_for_list:
-        if RESET_DB or consultee.cleaned_name is None:
-            org_name = consultee.name
+        org_name = consultee.name
 
-            org_name = Cleaner.get_organisation_name(org_name)
+        org_name = Cleaner.light_clean(org_name)
 
-            consultee.cleaned_name = org_name
-            Database.commit()
+        consultee.cleaned_name = org_name
+        Database.commit()
+    print(f'{document_index}/{nb_of_consultee_lists} - Cleaned')
+
+print('II-2 Deep cleaning organisation names from consultee lists...')
+for document_index, consultee_list in enumerate(saved_lists, start=1):
+    consultees_for_list = Consultee.query.filter_by(
+                                consultee_list_id=consultee_list.id
+                                                    ).all()
+
+    nb_of_consultee_lists = len(saved_lists)
+
+    for consultee in consultees_for_list:
+        org_name = consultee.cleaned_name
+
+        org_name = Cleaner.deep_clean(org_name)
+
+        consultee.cleaned_name = org_name
+        Database.commit()
     print(f'{document_index}/{nb_of_consultee_lists} - Cleaned')
--- service/cleaner.py
+++ service/cleaner.py
@@ -4,8 +4,12 @@ from service.database import Database
 class Cleaner(object):
 
     @staticmethod
-    def remove_leading_numbers(text):
+    def remove_leading_characters(text):
         return text.lstrip('0123456789abc.-_ abc ')
+
+    @staticmethod
+    def remove_trailing_characters(text):
+        return text.rstrip('0123456789.-_ ')
 
     @staticmethod
     def remove_line_breaks(text):
@@ -13,28 +17,34 @@ class Cleaner(object):
 
     @staticmethod
     def replace_by_popular_contained(text):
-        q = Database.get_popular_organisation_names(100)
+        q = Database.get_popular_organisation_names(250)
         popular_org_names = [r[0] for r in q]
 
         for popular_org_name in popular_org_names:
-            if popular_org_name in text and popular_org_name != text:
-                print(f'{text} -> {popular_org_name}')
-                text = popular_org_name
+            if popular_org_name in text:
+                if popular_org_name != text:
+                    print(f'{text} -> {popular_org_name}')
+                    text = popular_org_name
                 break
         return text
 
     @staticmethod
-    def get_organisation_name(text):
-        text = Cleaner.remove_leading_numbers(text)
+    def light_clean(text):
+        text = Cleaner.remove_leading_characters(text)
+        text = Cleaner.remove_trailing_characters(text)
+        return text
+
+    @staticmethod
+    def deep_clean(text):
         text = Cleaner.replace_by_popular_contained(text)
         return text
 
     @staticmethod
     def is_consultee_list(filename):
-        return 'Remissinstanser' in filename or \
-               'Remissmissiv' in filename or \
-               'Remisslista' in filename or \
-               'Remiss av' in filename
+        return any(s in filename for s in [ 'Remissmissiv',
+                                            'Remisslista',
+                                            'Remiss av'
+                                            ])
 
     @staticmethod
     def is_other_document(filename):
